d1a3aa050ad06d7ed9970c0b959162cd8315b368
Alexander Daza <dev.alexander.daza@gmail.com>
2023-05-30 19:04:01 -0500

    chore: implemented environment vars

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { join } from 'path';
+
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // make env variables global
+      envFilePath: join(__dirname, '..', '..', '..', '.env'), // path to .env file
+      validationSchema: Joi.object({
+        APP_PORT: Joi.number().default(3000),
+        APP_GLOBAL_PREFIX: Joi.string().default('api'),
+        APP_DATABASE: Joi.string().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

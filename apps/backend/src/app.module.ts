9bcb85e9507a8102d6a133cf90f2f59f6dccccc5
Alexander Daza <dev.alexander.daza@gmail.com>
2023-05-31 08:59:21 -0500

    chore: updated environment vars with connection db parameters

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -14,9 +14,15 @@ import { AppService } from './app.service';
       isGlobal: true, // make env variables global
       envFilePath: join(__dirname, '..', '..', '..', '.env'), // path to .env file
       validationSchema: Joi.object({
+        // Application
         APP_PORT: Joi.number().default(3000),
         APP_GLOBAL_PREFIX: Joi.string().default('api'),
-        APP_DATABASE: Joi.string().required(),
+        // Database
+        DB_HOST: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_URL: Joi.string().required(),
       }),
       validationOptions: {
         allowUnknown: true,

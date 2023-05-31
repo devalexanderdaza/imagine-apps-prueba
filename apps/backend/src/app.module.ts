b2581eb944782dc0d5f81638d175a88696cdb53c
Alexander Daza <dev.alexander.daza@gmail.com>
2023-05-31 09:01:46 -0500

    chore: base auth and user module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -7,6 +7,8 @@ import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -29,6 +31,8 @@ import { AppService } from './app.service';
         abortEarly: true,
       },
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

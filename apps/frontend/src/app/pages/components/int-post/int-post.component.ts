a174e6dbce1aa604b308d69ad2ce50feb9549a09
Alexander Daza <dev.alexander.daza@gmail.com>
2023-05-31 23:43:17 -0500

    feat: show all posts of logged user

--- apps/frontend/src/app/pages/components/int-post/int-post.component.ts
+++ apps/frontend/src/app/pages/components/int-post/int-post.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-int-post',
   templateUrl: './int-post.component.html',
-  styleUrls: ['./int-post.component.css']
+  styleUrls: ['./int-post.component.css'],
 })
 export class IntPostComponent {
-
+  @Input() title: string = '';
+  @Input() content: string = '';
+  @Input() createdAt: string = '';
 }
